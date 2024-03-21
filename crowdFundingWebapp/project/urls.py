b83b1696764de1e45c828b45e2728d448fd39889
sh-osama-sami <85364511+sh-osama-sami@users.noreply.github.com>
2024-03-21 13:44:14 +0200

    Delete crowdFundingWebapp/project directory

--- crowdFundingWebapp/project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('create_category/', create_category, name='create_category'),
-    path('select_featured_projects', select_featured_projects , name = 'select_featured_projects'),
-]
