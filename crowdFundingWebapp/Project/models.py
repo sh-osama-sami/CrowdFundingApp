1a98f3a6c1776c30eb9eaf9a1e3feee906b31b52
ember52 <117265490+ember52@users.noreply.github.com>
2024-03-21 16:03:32 +0200

    editing the 'project' model
    adding 'current_amount','total_rating ','rating_count ' fields into the project model

--- crowdFundingWebapp/Project/models.py
+++ crowdFundingWebapp/Project/models.py
@@ -15,6 +15,11 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     details = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    current_amount = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+
+    total_rating = models.IntegerField(default=0)
+    rating_count = models.IntegerField(default=0)
+
     total_target = models.DecimalField(max_digits=10, decimal_places=2)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
