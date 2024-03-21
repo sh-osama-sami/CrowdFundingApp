7f5e9bab43c5e2de18abaf0b9c6868da36c257f5
ember52 <117265490+ember52@users.noreply.github.com>
2024-03-21 15:07:56 +0200

    adding the necessary models for the 'project' application
    adding the 'Project','ProjectImage','Tag','ProjectTag'
    models to the project application to establish the tables necessary with the relations between each table.

--- crowdFundingWebapp/Project/models.py
+++ crowdFundingWebapp/Project/models.py
@@ -1,8 +1,8 @@
 from django.db import models
+from authentication.models import CustomUser
+
 
 # Create your models here.
-
-
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=False, null=False)
@@ -10,3 +10,34 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    details = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    total_target = models.DecimalField(max_digits=10, decimal_places=2)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    creator = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class ProjectImage(models.Model):
+    project = models.ForeignKey(Project, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='project_images/')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectTag(models.Model):
+    project = models.ForeignKey(Project, related_name='tags', on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
